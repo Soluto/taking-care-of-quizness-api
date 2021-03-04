55f522da940d817d40ef311b17fa42962c43cea4
Q <marquezesawyers@gmail.com>
2021-03-04 10:49:19 -0600

    update cognito scripts

--- scripts/02_createResourceServer.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-ID=''
-aws cognito-idp create-resource-server --region us-east-1 --user-pool-id ${ID} --identifier "quizResourceServer" --name "Quiz Application Resource Server" --scopes "ScopeName=questions.read,ScopeDescription=Get all questions" "ScopeName=questions.write,ScopeDescription=Create quiz question"
